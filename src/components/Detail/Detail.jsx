a56a0b76b085b3a8204d8635b0326c6855fa91c2
Luciano E. Perez <info.luciando.design@gmail.com>
2023-05-10 22:09:10 -0300

    detail estilos

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -1,60 +1,79 @@
 import styles from "./Detail.module.css";
 import axios from "axios";
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
+export default function Detail() {
+  const { id } = useParams();
+  const [character, setCharacter] = useState({});
 
-export default function Detail(){
+  const [loading, setLoading] = useState(true);
 
-    const { id } = useParams();
-    const [character, setCharacter] = useState({});
+  useEffect(() => {
+    axios(`https://rickandmortyapi.com/api/character/${id}`)
+      .then(({ data }) => {
+        if (data.name) {
+          setCharacter(data);
+        } else {
+          window.alert("No hay personajes con ese ID");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        setCharacter(null);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [id]);
 
-    const [loading, setLoading] = useState(true);
+  if (loading) {
+    return <div>Cargando...</div>;
+  }
 
-    useEffect(() => {
-        axios(`https://rickandmortyapi.com/api/character/${id}`)
-          .then(({ data }) => {
-            if (data.name) {
-              setCharacter(data);
-            } else {
-              window.alert('No hay personajes con ese ID');
-            }
-          })
-          .catch((error) => {
-            console.log(error);
-            setCharacter(null);
-          })
-          .finally(() => {
-            setLoading(false);
-          });
-      }, [id]);
-      
-      if (loading) {
-        return <div>Cargando...</div>;
-      }
-      
-      if (!character) {
-        return <div>Error: No se pudo cargar el personaje</div>;
-      }
-      
-      return (
-        <div className={styles.detailContent}>
-            <div>
-                <img src={character.image} alt={character.name} />
-            </div>
-          <div>
-          <h2>Name: {character.name}</h2>
-          <h2>Species: {character.species}</h2>
-          <h2>Status: {character.status}</h2>
-          <h2>Gender: {character.gender}</h2>
-          <h2>Origin: {character.origin.name}</h2>
-          <h2>Character number {character.id}</h2>
-          <h2>Shows up in {character.episode.length} {character.episode.length > 1 ? 'episodes' : 'episode'}</h2>
-          
-          <h2>Location: {character.location.name}</h2>
-          
-          
-          </div>
-        </div>
-      );
+  if (!character) {
+    return <div>Error: No se pudo cargar el personaje</div>;
+  }
+  const gender =
+    character.gender === "Male"
+      ? "His"
+      : character.gender === "Female"
+      ? "Her"
+      : "Its";
+  const pronoun =
+    character.gender === "Male"
+      ? "He"
+      : character.gender === "Female"
+      ? "She"
+      : "It";
+
+  return (
+    <div className={styles.detailContent}>
+      <div className = {styles.detailImg}>
+        <img src={character.image} alt={character.name} />
+      </div>
+      <div className={styles.detailData}>
+        <h2>{character.name}</h2>
+        <ul>
+          <li>
+            {gender} id number is {character.id}
+          </li>
+          <li>
+            {pronoun} is from {character.origin.name} and{" "}
+            {gender.toLowerCase()} current location is at{" "}
+            {character.location.name}.
+          </li>
+          <li>
+            {pronoun} is {character.status.toLowerCase()}{" "}
+            {character.status === "Alive" ? "and a" : "and was"}{" "}
+            {character.species.toLowerCase()}.
+          </li>
+          <li>
+            {pronoun} shows up in {character.episode.length}{" "}
+            {character.episode.length > 1 ? "episodes" : "episode"}.
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
