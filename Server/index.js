963395c76a73b6c1996d4886c5cf88c6128c73c5
Luciano E. Perez <info.luciando.design@gmail.com>
2023-06-01 16:20:55 -0300

    server

--- Server/index.js
+++ Server/index.js
@@ -1,16 +1,7 @@
-const http = require("http");
-const getCharById = require("./controllers/getCharById");
-
+const express = require('express');
+const server = express();
 const PORT = 3001;
-const server = http.createServer((req, res)=> {
-  const { url } = req;
-  console.log(`Server raised in port ${PORT}`);
-  res.setHeader('Access-Control-Allow-Origin', '*');
-    if (url.includes("/rickandmorty/character") ) {
-        const id = url.split("/").pop();
-        getCharById(res, id)
-    }
-        
-    }).listen(PORT, "localhost") ;
-    
-    module.exports = server;
+
+server.listen(PORT, () => {
+   console.log('Server raised in port: ' + PORT);
+});
